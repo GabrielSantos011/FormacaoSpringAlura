5cc9ad63e0a2db64d4865f5f72e22404fe08d93b
Gabriel <gabrielrafael.grs@gmail.com>
2021-09-06 19:14:35 -0300

    algumas configurações no web.xml e como definir duas rotas

--- Servelet/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/HelloWorldServlet.java
+++ Servelet/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/HelloWorldServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/oi")
+@WebServlet(urlPatterns = {"/oi", "/hello"})
 public class HelloWorldServlet extends HttpServlet {
 	
 	@Override
