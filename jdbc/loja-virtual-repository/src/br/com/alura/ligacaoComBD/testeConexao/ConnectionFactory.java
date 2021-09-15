0547c98cd1a92603dab12035ff6477f9f98d5ab4
Gabriel <gabrielrafael.grs@gmail.com>
2021-09-14 21:27:41 -0300

    colocando a porta da conexão

--- jdbc/loja-virtual-repository/src/br/com/alura/ligacaoComBD/testeConexao/ConnectionFactory.java
+++ jdbc/loja-virtual-repository/src/br/com/alura/ligacaoComBD/testeConexao/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     public Connection getConexao() throws SQLException {
         Connection conexao = DriverManager
-                .getConnection("jdbc:mysql://localhost/loja_virtual?userTimezone=true&serverTimezone=UTC",
+                .getConnection("jdbc:mysql://localhost:3306/loja_virtual?userTimezone=true&serverTimezone=UTC",
                         "root", "123456");
 
         return conexao;
